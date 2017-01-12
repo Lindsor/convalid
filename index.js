3855ae8e79481018abc62a161dec94c5f2d9e7ff
lindsor <marcelopluz@gmail.com>
2017-01-11 22:21:25 -0500

    Add the isBoolean check

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ module.exports = exports = {
   isEmpty: function(value) {
 
     /* Double equal check for null */
-    if (value == null) return true;
+    if (this.isUndefined(value)) return true;
 
     /* Cast to string */
     value = value + "";
@@ -57,7 +57,78 @@ module.exports = exports = {
     return value.length === 0;
   },
 
-  isNumber: notImplemented,
+  /**
+   * Checks if the passed in value is a number or not.
+   * @param  {Mixed}    value  The value to check if its a number.
+   * @return {Boolean}         True if a number, false otherwise.
+   */
+  isNumber: function(value) {
+
+    /* Booleans can be casted to numbers so catch this first */
+    if (this.isBoolean(value)) return false;
+
+    /**
+     * Number([<Number>]) returns a number.
+     * It returns:
+     *   0   | If the array has no elements
+     *   1   | If the array has 1 element
+     *   NaN | If the array has more then 1 element
+     */
+    if (Array.isArray(value)) return false;
+
+    /**
+     * Number(null) returns a number.
+     * Force a undefined check.
+     */
+    if (this.isUndefined(value)) return false;
+
+    /**
+     * Number("") returns a number.
+     * Force an empty check.
+     */
+    if (this.isEmpty(value)) return false;
+
+    /* Cast to a number */
+    value = Number(value);
+
+    /**
+     * NaN can be the result of a Number() call.
+     * It is the only thing in JS that returns true for:
+     *   NaN !== NaN
+     */
+    if (value !== value) return false;
+
+    return typeof value === "number";
+
+  },
+
+  /**
+   * Checks if the passed in value is undefined or null.
+   * @param  {Mixed}   value The value to check for undefined.
+   * @return {Boolean}       True if undefined or null, false otherwise.
+   */
+  isUndefined: function(value) {
+
+    /* 2 equality check for undefined */
+    return (value == null);
+
+  },
+
+  /**
+   * Checks if the passed in value is a boolean or not.
+   * @param  {Mixed}  value  The value to check weather its a boolean or not.
+   * @return {Boolean}       True if its a boolean, false otherwise.
+   */
+  isBoolean: function(value) {
+
+    var type = typeof value;
+
+    if (type === "boolean") return true;
+
+    return (value instanceof Boolean);
+
+  },
+
   isGreaterThan: notImplemented,
   isLessThan: notImplemented,
   isEmail: notImplemented,
--- test/convalid.js
+++ test/convalid.js
@@ -32,7 +32,8 @@ describe("convalid", function() {
         hello: "what"
       }, -1, [1],
       Infinity,
-      new Number(2)
+      new Number(2),
+      new Boolean(true)
     ];
 
     it("should be a function", function() {
@@ -57,6 +58,144 @@ describe("convalid", function() {
 
   });
 
+  describe("#isNumber", function() {
+
+    const pass = [
+      1,
+      0, -1,
+      2.3, -56.7,
+      Infinity,
+      new Number(2)
+    ];
+
+    const fail = [
+      NaN,
+      "hello",
+      false,
+      new Boolean(true),
+      true, {
+        hello: "what"
+      },
+      [1],
+      "",
+      undefined,
+      null
+    ];
+
+    it("should be a function", function() {
+      assert.isFunction(convalid.isNumber);
+    });
+
+    it("should return a boolean", function() {
+      assert.isBoolean(convalid.isNumber("hello"));
+    });
+
+    pass.forEach(pass => {
+      it(`should return true for '${pass}'`, function() {
+        assert.equal(convalid.isNumber(pass), true);
+      });
+    });
+
+    fail.forEach(fail => {
+      it(`should return false for '${fail}'`, function() {
+        assert.equal(convalid.isNumber(fail), false);
+      });
+    });
+
+  });
+
+  describe("#isUndefined", function() {
+
+    const pass = [
+      undefined,
+      null
+    ];
+
+    const fail = [
+      NaN,
+      1,
+      0, -1,
+      2.3, -56.7,
+      Infinity,
+      new Number(2),
+      "hello",
+      false,
+      new Boolean(true),
+      true, {
+        hello: "what"
+      },
+      [1],
+      ""
+    ];
+
+    it("should be a function", function() {
+      assert.isFunction(convalid.isUndefined);
+    });
+
+    it("should return a boolean", function() {
+      assert.isBoolean(convalid.isUndefined("hello"));
+    });
+
+    pass.forEach(pass => {
+      it(`should return true for '${pass}'`, function() {
+        assert.equal(convalid.isUndefined(pass), true);
+      });
+    });
+
+    fail.forEach(fail => {
+      it(`should return false for '${fail}'`, function() {
+        assert.equal(convalid.isUndefined(fail), false);
+      });
+    });
+
+  });
+
+  describe("#isBoolean", function() {
+
+    const pass = [
+      false,
+      new Boolean(true),
+      true
+    ];
+
+    const fail = [
+      NaN,
+      undefined,
+      null,
+      1,
+      0, -1,
+      2.3, -56.7,
+      Infinity,
+      new Number(2),
+      "hello", {
+        hello: "what"
+      },
+      [1],
+      ""
+    ];
+
+    it("should be a function", function() {
+      assert.isFunction(convalid.isBoolean);
+    });
+
+    it("should return a boolean", function() {
+      assert.isBoolean(convalid.isBoolean("hello"));
+    });
+
+    pass.forEach(pass => {
+      it(`should return true for '${pass}'`, function() {
+        assert.equal(convalid.isBoolean(pass), true);
+      });
+    });
+
+    fail.forEach(fail => {
+      it(`should return false for '${fail}'`, function() {
+        assert.equal(convalid.isBoolean(fail), false);
+      });
+    });
+
+  });
+
   describe("#passRequired", function() {
 
     const pass = [
@@ -64,6 +203,7 @@ describe("convalid", function() {
       1,
       0,
       false,
+      new Boolean(true),
       true, {
         hello: "what"
       }, -1, [1],
